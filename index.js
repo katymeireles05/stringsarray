9bf1419737f12ee9887d7f50b6a937537abf17b1
Jean Marzari <jeanmarzari@hotmail.com>
2023-04-28 21:11:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// const nome = prompt("Nome?:")
+// console.log(`o "${nome}" é turista`); ,
+// 
+// const nome = prompt ("nome")
+// const cor = prompt ("cor preferida")
+// console.log("A cor preferida, nome "é", cor)
+
+// const nome = prompt("Digite seu nome")
+// nome.length  === 0 ?
+// console.log("Voce precisa digitar algo");
+// console.log("o nome é" ,nome);
+
+// const sobrenome = "Zukeneberg"
+// console.log(sobrenome.toLowerCase())
+// console.log(sobrenome.toUpperCase())
+
+// const nome = "maria"
+// console.log(nome.trim());
+// const frase = "Hoje comi cenoura"
+// console.log(frase.includes("cenouras"));
+
+// receita = "bolo de CENOURA"
+// const novaReceita = receita.replace("abacaxi","cenoura")
+// console.log(novaReceita)
+// console.log(frase.trim().toLowerCase().includes(cenoura))
+
+const nome = "meireles"
+console.log(nome.toUpperCase())
+
+const receita ="bolo de chocolate"
+const novaReceita = receita.replaceA11("o", "i")
